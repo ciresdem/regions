1cd18bf7143453ed7034a39891658843da8d96bc
Matthew Love <matthew.love@colorado.edu>
2019-11-15 18:15:27 -0800

    x to b

--- src/regions.c
+++ src/regions.c
@@ -19,9 +19,10 @@
 #include <stddef.h>
 //#include "regions.h"
 
-#define REGIONS_VERSION "0.0.1"
+#define REGIONS_VERSION "0.0.2"
 
-typedef struct {
+typedef struct 
+{
   double xmin;
   double xmax;
   double ymin;
@@ -36,7 +37,8 @@ static int help_flag;
 static int verbose_flag;
 
 static void
-print_version(const char* command_name, const char* command_version) {
+print_version(const char* command_name, const char* command_version) 
+{
   fprintf(stderr, "%s %s \n\
 Copyright © 2019 Matthew Love <matthew.love@colorado.edu> \n\
 %s is liscensed under the GPL v.2 or later and is \n\
@@ -54,49 +56,54 @@ regions [OPTION]... -R<region> -R<region>... \n\
 Manipulate regions.\n\
 \n\
   -R, --region\t\tthe input region ( xmin/xmax/ymin/ymax )\n\
-  -m, --merge\t\tmerge the input region(s)\n\
+  -b, --buffer\t\tbuffer the region by value\n\
   -e, --echo\t\techo the (processed) region(s)\n\
+  -m, --merge\t\tmerge the input region(s)\n\
   -n, --name\t\techo the (processed) region(s) as a name-string\n\n\
       --verbose\t\tincrease the verbosity.\n\
       --help\t\tprint this help menu and exit.\n\
       --version\t\tprint version information and exit.\n\
 \n\
 ");
-  exit(1);
+  exit (1);
 }
 
 int
-region_p (region_t *region) {
+region_p (region_t *region) 
+{
   if (region->xmin >= region->xmax) return 0;
   if (region->ymin >= region->ymax) return 0;
   return 1;
 }
 
 void
-region_echo (region_t *region) {
-  printf("-R%f/%f/%f/%f\n", region->xmin, region->xmax, region->ymin, region->ymax);
+region_echo (region_t *region, int eflag) 
+{
+  if (eflag == 2) printf("%f/%f/%f/%f\n", region->xmin, region->xmax, region->ymin, region->ymax);
+  else printf("-R%f/%f/%f/%f\n", region->xmin, region->xmax, region->ymin, region->ymax);
 }
 
 void
-region_name (region_t *region) {
+region_name (region_t *region) 
+{
   char* ns;
   char* ew;
   if (region->ymax < 0) ns = "s";
   else ns = "n";
   if (region->xmin < 0) ew = "w";
   else ew = "s";
-  printf("%s%02dx%02d_%s%03dx%02d\n", \
-	 ns, abs((int)region->ymax), abs(((int)(region->ymax*100)%100)), \
+  printf("%s%02dx%02d_%s%03dx%02d\n",
+	 ns, abs((int)region->ymax), abs(((int)(region->ymax*100)%100)),
 	 ew, abs((int)region->xmin), abs((int)(region->xmin*100)%100));
 }
 
 void
-region_format (region_t *region, int aflag) {
+region_format (region_t *region, int aflag) 
+{
   /* This is for the GMT compatibility. More can be done here.
    */
-  if (aflag == 0) {
+  if (aflag == 0)
     printf("# @VGMT1.0 @GMULTIPOLYGON\n# @NName\n# @Tstring\n# FEATURE_DATA\n");
-  }
   printf(">\n# @D%s\n# @P\n", "regions");
   printf("%f %f\n", region->xmin, region->ymax);
   printf("%f %f\n", region->xmax, region->ymax);
@@ -106,24 +113,29 @@ region_format (region_t *region, int aflag) {
 }
 
 void
-region_merge (region_t *regions, int rsize) {
+region_merge (region_t *regions, int rsize) 
+{
   int i;
-  for (i=0; i < rsize; i++) {
-    if (i > 0){
-      if (regions[i].xmin < regions[0].xmin) 
-	regions[0].xmin = regions[i].xmin;
-      if (regions[i].xmax > regions[0].xmax)
-	regions[0].xmax = regions[i].xmax;
-      if (regions[i].ymin < regions[0].ymin) 
-	regions[0].ymin = regions[i].ymin;
-      if (regions[i].ymax > regions[0].ymax)
-	regions[0].ymax = regions[i].ymax;
-    }
-  }
+  for (i=0; i < rsize; i++) 
+    if (i > 0)
+      {
+	if (regions[i].xmin < regions[0].xmin) 
+	  regions[0].xmin = regions[i].xmin;
+
+	if (regions[i].xmax > regions[0].xmax)
+	  regions[0].xmax = regions[i].xmax;
+
+	if (regions[i].ymin < regions[0].ymin) 
+	  regions[0].ymin = regions[i].ymin;
+
+	if (regions[i].ymax > regions[0].ymax)
+	  regions[0].ymax = regions[i].ymax;
+      }
 }
 
 void
-region_extend (region_t *region, double xval) {
+region_extend (region_t *region, double xval) 
+{
   region->xmin = region->xmin - xval;
   region->xmax = region->xmax + xval;
   region->ymin = region->ymin - xval;
@@ -131,118 +143,122 @@ region_extend (region_t *region, double xval) {
 }
 
 region_t
-region_parse (char* region_string) {
+region_parse (char* region_string) 
+{
 
   int j;
   region_t rgn;
 
-  char* p = strtok(region_string, "/");
+  char* p = strtok (region_string, "/");
   for (j = 0; j < 4; j++) {
     if (p != NULL) {
-      if (j==0) rgn.xmin = atof(p);
-      if (j==1) rgn.xmax = atof(p);
-      if (j==2) rgn.ymin = atof(p);
-      if (j==3) rgn.ymax = atof(p);
+      if (j==0) rgn.xmin = atof (p);
+      if (j==1) rgn.xmax = atof (p);
+      if (j==2) rgn.ymin = atof (p);
+      if (j==3) rgn.ymax = atof (p);
     }
-    p = strtok(NULL, "/");
+    p = strtok (NULL, "/");
   }
   return rgn;
 }
   
 
 int
-main (int argc, char **argv) {
+main (int argc, char **argv) 
+{
 
   int c, status;
-  int rflag = 0, mflag = 0, eflag = 0, nflag = 0, xflag = 0;
+  int rflag = 0, mflag = 0, eflag = 0, nflag = 0, bflag = 0;
   char* region;
   char* tmp_region;
-  double xval;
+  double bval;
   int pf_length;
   ssize_t n = 0;
 
-  char REGION_D[]="-180/180/-90/90";
-  char REGION_G[]="0/360/-90/90";
+  char REGION_D[] = "-180/180/-90/90";
+  char REGION_G[] = "0/360/-90/90";
 
   region_t* rgns;
-  rgns = (region_t*) malloc(sizeof(region_t));
+  rgns = (region_t*) malloc (sizeof (region_t));
 
-  while (1) {
-    static struct option long_options[] =
-      {
-	/* These options set a flag. */
-	{"version", no_argument, &version_flag, 1},
-	{"help", no_argument, &help_flag, 1},
-	{"verbose", no_argument, &verbose_flag, 1},
-	/* These options don't set a flag.
-	   We distinguish them by their indices. */
-	{"region", required_argument, 0, 'R'},
-	{"merge", no_argument, 0, 'm'},
-	{"echo", no_argument, 0, 'e'},
-	{"name", no_argument, 0, 'n'},
-	{"extend", required_argument, 0, 'x'},
-	{0, 0, 0, 0}
-      };
-    /* getopt_long stores the option index here. */
-    int option_index = 0;
-    
-    c = getopt_long (argc, argv, "R:menx:",
-		     long_options, &option_index);
-    
-    /* Detect the end of the options. */
-    if (c == -1) break;
-    
-    switch (c) {
-    case 0:
-      /* If this option set a flag, do nothing else now. */
-      if (long_options[option_index].flag != 0)
-	break;
-      printf ("option %s", long_options[option_index].name);
-      if (optarg)
-	printf (" with arg %s", optarg);
-      printf ("\n");
-      break;
-    case 'R':
-      n++;
-
-      tmp_region = optarg;
-
-      if (strcmp(tmp_region, "d") == 0) region = REGION_D;
-      else if (strcmp(tmp_region, "g") == 0) region = REGION_G;
-      else region = tmp_region;
-
-      region_t* temp = realloc(rgns, n * sizeof(region_t));
-      rgns = temp;
-
-      rgns[rflag] = region_parse(region);
-      rflag++;
-      break;
-    case 'm':
-      mflag++;
-      break;
-    case 'e':
-      eflag++;
-      break;
-    case 'n':
-      nflag++;
-      break;
-    case 'x':
-      xflag++;
-      xval = atof(optarg);
-      break;
+  while (1) 
+    {
+      static struct option long_options[] =
+	{
+	  /* These options set a flag. */
+	  {"version", no_argument, &version_flag, 1},
+	  {"help", no_argument, &help_flag, 1},
+	  {"verbose", no_argument, &verbose_flag, 1},
+	  /* These options don't set a flag.
+	     We distinguish them by their indices. */
+	  {"region", required_argument, 0, 'R'},
+	  {"merge", no_argument, 0, 'm'},
+	  {"echo", no_argument, 0, 'e'},
+	  {"name", no_argument, 0, 'n'},
+	  {"buffer", required_argument, 0, 'b'},
+	  {0, 0, 0, 0}
+	};
+      /* getopt_long stores the option index here. */
+      int option_index = 0;
       
-    case '?':
-      /* getopt_long already printed an error message. */
-      fprintf(stderr,"Try 'regions --help' for more information.\n");
-      exit(0);
-      break;
+      c = getopt_long (argc, argv, "R:menb:",
+		       long_options, &option_index);
       
-    default:
-      abort ();
+      /* Detect the end of the options. */
+      if (c == -1) break;
+      
+      switch (c) 
+	{
+	case 0:
+	  /* If this option set a flag, do nothing else now. */
+	  if (long_options[option_index].flag != 0)
+	    break;
+	  printf ("option %s", long_options[option_index].name);
+	  if (optarg)
+	    printf (" with arg %s", optarg);
+	  printf ("\n");
+	  break;
+	case 'R':
+	  n++;
+	  
+	  tmp_region = optarg;
+	  
+	  if (strcmp (tmp_region, "d") == 0) region = REGION_D;
+	  else if (strcmp (tmp_region, "g") == 0) region = REGION_G;
+	  else region = tmp_region;
+	  
+	  region_t* temp = realloc (rgns, n * sizeof (region_t));
+	  rgns = temp;
+	  
+	  rgns[rflag] = region_parse (region);
+	  rflag++;
+	  break;
+	case 'm':
+	  mflag++;
+	  break;
+	case 'e':
+	  eflag++;
+	  break;
+	case 'n':
+	  nflag++;
+	  break;
+	case 'b':
+	  bflag++;
+	  bval = atof (optarg);
+	  break;
+	  
+	case '?':
+	  /* getopt_long already printed an error message. */
+	  fprintf(stderr,"Try 'regions --help' for more information.\n");
+	  exit(0);
+	  break;
+	  
+	default:
+	  abort ();
+	}
     }
-  }
 
-  if (version_flag) print_version("regions", REGIONS_VERSION);
+  if (version_flag) print_version ("regions", REGIONS_VERSION);
   if (help_flag) usage();
 
   if (!rflag) usage();
@@ -251,21 +267,26 @@ main (int argc, char **argv) {
 
   // merge regions
   if (mflag) {
-    region_merge(rgns,rflag);
+    region_merge (rgns,rflag);
     rflag = 1;
   }
 
   // parse through regions and print them to stdout.
-  for (i=0; i<rflag; i++) {
-    if (region_p(&rgns[i])) {
-      if (xflag) region_extend(&rgns[i], xval);
-      if (eflag) region_echo(&rgns[i]);
-      else if (nflag) region_name(&rgns[i]);
-      else region_format(&rgns[i], i);
-    }
-  }
+  for (i=0; i<rflag; i++) 
+    if (region_p(&rgns[i])) 
+      {
+	if (bflag) 
+	  region_extend (&rgns[i], bval);
 
-  free(rgns);
-  rgns=NULL;
-  exit(1);
+	if (eflag) 
+	  region_echo (&rgns[i], eflag);
+	else if (nflag) 
+	  region_name (&rgns[i]);
+	else 
+	  region_format (&rgns[i], i);
+      }
+
+  free (rgns);
+  rgns = NULL;
+  exit (1);
 }
